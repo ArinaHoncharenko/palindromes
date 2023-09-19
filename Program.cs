1f93c98fd23caecdc1848213d57543f441f0851b
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-09-19 21:11:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace oop3_palindrome
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Enter a word or phrase to check for palindromicity: ");
+                string userInput = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Input is empty.\n");
+                }
+                else if (PalindromeChecker.IsPalindrome(userInput))
+                {
+                    Console.WriteLine(userInput + " is a palindrome.\n");
+                }
+                else
+                {
+                    Console.WriteLine(userInput + " isn't a palindrome.\n");
+                }
+            }
+        }
+    }
+}
---
+++ methods.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+
+namespace oop3_palindrome 
+{
+    class PalindromeChecker
+    {
+        public static bool IsPalindrome(string input)
+        {
+            // Видаляємо символи розділення і змінюємо регістр на нижній
+            string cleanInput = input.Replace(" ", "").ToLower();
+
+            // Перевіряємо, чи містить слово цифри
+            if (cleanInput.Any(char.IsDigit))
+            {
+                Console.WriteLine(input + " contains numbers.");
+                return false;
+            }
+
+            int left = 0;
+            int right = cleanInput.Length - 1;
+
+            // Перевірка на паліндромність
+            while (left < right)
+            {
+                if (cleanInput[left] != cleanInput[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
